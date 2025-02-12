003d6b25755f8f0cbba626d7ef4e62bf007a8ee3
Samuele Converso <samu.converso@gmail.com>
2025-02-12 16:56:17 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio_U4_S1_L3/Models/ContoCorrente.cs
@@ -0,0 +1,47 @@
+﻿namespace Esercizio_U4_S1_L3.Models {
+    public class ContoCorrente {
+
+        public string NomeConto {
+            get; set;
+        } = "";
+
+        public decimal SaldoConto {
+            get; set;
+        } = 0;
+
+        public bool ContoAperto {
+            get; set;
+        } = false;
+
+        public ContoCorrente(string nomeConto, decimal saldoConto) {
+            NomeConto = nomeConto;
+            if (saldoConto < 1000) {
+                Console.WriteLine("Il saldo iniziale del conto non può essere inferiore a 1000 euro.");
+            } else {
+                SaldoConto += saldoConto;
+                ContoAperto = true;
+            }
+
+        }
+
+        public void ToggleConto() {
+            ContoAperto = !ContoAperto;
+        }
+
+        public void Versa(decimal n) {
+            if (ContoAperto) {
+                SaldoConto += n;
+            } else {
+                Console.WriteLine("Il conto è chiuso.");
+            }
+        }
+
+        public void Preleva(decimal n) {
+            if (ContoAperto && SaldoConto >= n) {
+                SaldoConto -= n;
+            } else {
+                Console.WriteLine("Il conto è chiuso oppure non hai abbastanza soldi nel conto per prelevare tale quantità.");
+            }
+        }
+    }
+}
---
+++ Esercizio_U4_S1_L3/Program.cs
@@ -0,0 +1,112 @@
+﻿using Esercizio_U4_S1_L3.Models;
+
+
+// ESERCIZIO 1
+
+
+
+
+
+
+
+
+//Console.WriteLine("Ciao! Vuoi aprire un conto corrente? (s/n)");
+//var apriConto = Console.ReadLine()?.ToLower();
+//var nomeUtenteConto = "";
+//var saldoIniziale = 0;
+//ContoCorrente nuovoConto;
+
+//if (apriConto == "s") {
+//    Console.WriteLine("Come ti chiami? Questo sarà anche il nome del tuo conto.");
+//    nomeUtenteConto = Console.ReadLine();
+//    Console.WriteLine("Qual è il tuo saldo iniziale?");
+//    saldoIniziale = int.Parse(Console.ReadLine()!);
+//    nuovoConto = new ContoCorrente(nomeUtenteConto!, saldoIniziale);
+
+//    if (nuovoConto.SaldoConto == 0) {
+//        Environment.Exit(0);
+//    }
+
+////
+//repeat:
+//    Console.WriteLine("Cosa vuoi fare ora? (versa/preleva/*)");
+//    var azione = Console.ReadLine()?.ToLower();
+//    if (azione == "versa") {
+//        Console.WriteLine("Quanto vuoi versare?");
+//        var versamento = int.Parse(Console.ReadLine()!);
+//        nuovoConto.Versa(versamento);
+//        Console.WriteLine($"Il tuo saldo ora ammonta a: {nuovoConto.SaldoConto}");
+//        goto repeat;
+//    } else if (azione == "preleva") {
+//        Console.WriteLine("Quanto vuoi prelevare?");
+//        var prelevamento = int.Parse(Console.ReadLine()!);
+//        nuovoConto.Preleva(prelevamento);
+//        Console.WriteLine($"Il tuo saldo ora ammonta a: {nuovoConto.SaldoConto}");
+//        goto repeat;
+//    } else {
+//        Console.WriteLine("Arrivederci!");
+//        return;
+//    }
+//} else {
+//    Console.WriteLine("Arrivederci!");
+//    return;
+//}
+
+
+
+
+
+
+// ESERCIZIO 2
+
+//string[] nomi = new string[] { "Samuele", "Flavio", "Gianfranco" };
+
+//void verificaNome(string[] arr, string str) {
+//    bool trovato = false;
+//    for (int i = 0; i < nomi.Length; i++) {
+//        if (arr[i] == str) {
+//            trovato = true;
+//            break;
+//        } else {
+//            trovato = false;
+//        }
+//    }
+//    if (trovato) {
+//        Console.WriteLine("Il nome è stato trovato!");
+//    }
+//}
+
+//verificaNome(nomi, "Flavio");
+
+
+
+
+
+
+// ESERCIZIO 3
+
+Console.WriteLine("Inserisci dimensione array:");
+var dimensione = int.Parse(Console.ReadLine()!);
+
+int[] nums = new int[dimensione];
+
+for (int i = 0; i < dimensione; i++) {
+    Random num = new Random();
+    nums[i] = num.Next(1, 100);
+}
+
+void StampaSommaEMedia(int[] arr) {
+    int somma = 0;
+    int media = 0;
+
+    for (int i = 0; i < dimensione; i++) {
+        somma += arr[i];
+    }
+
+    media = somma / dimensione;
+
+    Console.WriteLine("La somma degli elementi dell'array è: " + somma);
+    Console.WriteLine("La media degli elementi dell'array è: " + media);
+}
+
+StampaSommaEMedia(nums);
